23cf8b79ea2d9619202f3a59ffa821fa457ec549
Omar <omarvfais@gmail.com>
2024-03-01 22:04:45 +0530

    Data Models

--- techkart/customers/models.py
+++ techkart/customers/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+# Model for customers
+class Customer(models.Model):
+    LIVE = 1
+    DELETE = 0
+    DELETE_CHOICES = ((LIVE, "live"), (DELETE, "delete"))
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+    user = models.OneToOneField(
+        User, related_name="customer_profile", on_delete=models.CASCADE
+    )
+    phone = models.IntegerField(default=0)
+    delete_status = models.IntegerField(choices=DELETE_CHOICES, default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
