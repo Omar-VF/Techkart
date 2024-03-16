efc9efb521f420d1931093a8a08d9dc1a330bf7c
Omar <omarvfais@gmail.com>
2024-03-16 19:46:57 +0530

    migrations

--- techkart/techkart/settings.py
+++ techkart/techkart/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-_alh6a*^8*)48-&-3ccibd(-qd!9za#s97(&)!fwhelxmezjq="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".vercel.app",'127.0.0.1']
 
